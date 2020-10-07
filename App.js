b71ebd5ebcadfa8e1926320cfcb8d0bbd50ee7af
Yokar <yokar@protonmail.com>
2020-10-07 01:34:28 -0500

    Icon

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 //Home
 const HomeScreen = ({ navigation }) => {
@@ -20,25 +22,13 @@ const HomeScreen = ({ navigation }) => {
 
 HomeScreen.navigationOptions = {
   title: 'Inicio',
-  /*headerRight: () => (
-    <Button
-      onPress={() => alert('Lalalalala')}
-      title="Soy lala"
-      color='#222' />
-  ),*/
-  /*headerStyle: {
-    backgroundColor: '#ffeecc'
-  },
-  headerTintColor: '#222',
-  headerTitleStyle: {
-    fontWeight:'900'
-  }*/
 }
 
 //Detalle
 const DetalleScreen = ({ navigation }) => {
   const [cont, setCont] = useState(0)
   const incrementar = () => setCont(cont + 1)
+
   useEffect(() => {
     navigation.setParams({ incrementar })
   }, [cont])
@@ -50,7 +40,7 @@ const DetalleScreen = ({ navigation }) => {
       <Text>Soy la pantalla de detalle {cont} </Text>
         <Button
           title="Volver"
-          onPress={() => navigation.setParams({ title: 'Usuario 1' })}
+          onPress={() => navigation.navigate('modal')}
           />
       <StatusBar style="auto" />
     </View>
@@ -58,7 +48,7 @@ const DetalleScreen = ({ navigation }) => {
 }
 
 //Navigator
-DetalleScreen.navigationOptions = ({ navigation, navigationOptions }) => {
+DetalleScreen.navigationOptions = ({ navigation }) => {
   return{
     title: navigation.getParam('title', 'Cargando...'),
     headerRight: () => (
@@ -67,12 +57,11 @@ DetalleScreen.navigationOptions = ({ navigation, navigationOptions }) => {
         title="Más uno"
         color='#555' />
     ),
-    //headerTintColor: '#5e5'
   }
 }
 
 //Componente de navegación
-const AppNavigator = createStackNavigator({
+const AppNavigator = createBottomTabNavigator({
   Home: {
     screen: HomeScreen
   },
@@ -80,19 +69,41 @@ const AppNavigator = createStackNavigator({
     screen: DetalleScreen
   }
 } , { initialRouteName: 'Home',
-      defaultNavigationOptions:{
-        headerStyle: {
-          backgroundColor: '#ffeecc'
-        },
-        headerTintColor: '#555',
-        headerTitleStyle: {
-          fontWeight: 'bold'
-        }
-      }
+      defaultNavigationOptions: ({ navigation }) => ({
+          tabBarIcon:({ focused, horizontal, tintColor }) => {
+            const { routeName } = navigation.state
+            let iconName
+            if ( routeName === 'Home' ){
+              iconName = `ios-information-circle${focused ? '' : '-outline'}`
+            } else {
+              iconName = 'ios-options'
+            }
+
+            return <Ionicons name={iconName} size={20} tintColor={tintColor} />
+          },
+          tabBarOptions:{
+            activeTintColor: navigation.state.routeName == "Home" ? '#e91e63' : 'orange',
+            inactiveTintColor: 'black',
+            labelStyle: {
+              fontSize: 16,
+            },
+            style:{
+              backgroundColor: '#fec'
+            }
+          }
+      })
 } );
 
+const RootStack = createStackNavigator({
+  Main: AppNavigator,
+  MiModal: () => <Text>lALALAL</Text>
+},{
+  mode: 'modal',
+  headerMode: 'none',
+})
+
 //Exportación de componente
-export default createAppContainer(AppNavigator)
+export default createAppContainer(RootStack)
 
 const styles = StyleSheet.create({
   container: {
